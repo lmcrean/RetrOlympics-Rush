05dfb9eba9c209aafb842e0a7dbba72ea28a2b50
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-08-18 15:36:15 +0100

    Add button function

--- script.js
+++ script.js
@@ -12,6 +12,53 @@ kaboom({
 loadSprite("athlete", "assets/sprites/man.png");
 loadSprite("background", "assets/sprites/backgroundtwo.jpg"); 
  
+function addButton(txt, p, f) {
+
+	// add a parent background object
+	const btn = add([
+		rect(240, 80, { radius: 8 }),
+		pos(p),
+		area(),
+		scale(1),
+		anchor("center"),
+		outline(4),
+	])
+
+	// add a child object that displays the text
+	btn.add([
+		text(txt),
+		anchor("center"),
+		color(0, 0, 0),
+	])
+
+	// onHoverUpdate() comes from area() component
+	// it runs every frame when the object is being hovered
+	btn.onHoverUpdate(() => {
+		const t = time() * 10
+		btn.color = hsl2rgb((t / 10) % 1, 0.6, 0.7)
+		btn.scale = vec2(1.2)
+		setCursor("pointer")
+	})
+
+	// onHoverEnd() comes from area() component
+	// it runs once when the object stopped being hovered
+	btn.onHoverEnd(() => {
+		btn.scale = vec2(1)
+		btn.color = rgb()
+	})
+
+	// onClick() comes from area() component
+	// it runs once when the object is clicked
+	btn.onClick(f)
+
+	return btn
+
+}
+
+addButton("Start", vec2(700, 600), () => {
+    go("game");
+});
+
 scene("game", () => {
 
     // Draw the background image onto the canvas
